739ae1683e079d8145e15fe7efaa0acb4e9e3142
sabrina <sabriinarayssa62@gmail.com>
2025-09-08 09:14:01 -0300

    Adicionando projeto

---
+++ src/services/schedule-cancel.js
@@ -0,0 +1,17 @@
+import {apiConfig} from "./api-config.js"
+
+export async function scheduleCancel(id) {
+  try {
+    await fetch(`${apiConfig.baseURL}/schedules/${id}`,{
+   method: "DELETE",
+   headers: { 
+      "Content-Type": "application/json",
+    },
+  })
+
+  alert("Agendamento cancelado com sucesso!")
+}catch (error){
+  console.log(error)
+  alert("Não foi possível cancelar o agendamento.")
+ }
+}
